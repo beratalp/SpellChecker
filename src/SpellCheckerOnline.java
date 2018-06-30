edc2ca38a5e3a654bd60797527ce2fc6a3bafe0a
beratalp <beratalp@gmail.com>
2018-07-01 00:08:22 +0300

    improved online mode detection: now with os detection xd

--- src/SpellCheckerOnline.java
+++ src/SpellCheckerOnline.java
@@ -3,7 +3,14 @@ import java.io.*;
 
 public class SpellCheckerOnline extends SpellChecker {
     public static boolean isConnectionWorks() throws InterruptedException, IOException {
-        Process p1 = Runtime.getRuntime().exec("ping -c 1 www.google.com");
+        String command;
+        if (System.getProperty("os.name").startsWith("Windows")){
+            command = "ping -n 1 www.google.com";
+        }
+        else{
+            command = "ping -c 1 www.google.com";
+        }
+        Process p1 = Runtime.getRuntime().exec(command);
         int returnVal = p1.waitFor();
         boolean reachable = (returnVal==0);
         return reachable;
