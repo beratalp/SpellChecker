ee6a4ad0410f2917e0a1212cd7d45bc39e8f6ae3
beratalp <beratalp@gmail.com>
2018-07-10 14:52:02 +0300

    initial works of inheritance

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -5,4 +5,13 @@ import java.net.UnknownHostException;
 public abstract class SpellChecker {
     final static String VERSION = "0.1";
     final static String APPNAME = "Spell Inspector";
+
+    public enum Language{
+        TURKISH,
+        ENGLISH
+    }
+
+    public abstract int[] spellCheck(String str, Language lang);
+
+    public abstract int[] grammarCheck(String str, Language lang);
 }
