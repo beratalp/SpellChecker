fb264e58d4419ade078915dbfb0b6c7bd49986ef
beratalp <beratalp@gmail.com>
2018-07-24 22:28:00 +0300

    fixed some bugs

--- src/TextMain.java
+++
@@ -1,7 +0,0 @@
-
-import java.io.*;
-public class TextMain {
-    public static void main (String[] args) throws IOException{
-        //TextFrame frame = new TextFrame(new File("recent.txt"));
-    }
-}
